354f674053e2d209b4c4f93188029682b0dfaf4f
Danielle-Luz <daniln2209@gmail.com>
2023-05-07 17:44:49 +0000

    feat: create bot-data file and change post question message

--- commands/commands-definition/new-question.js
+++ commands/commands-definition/new-question.js
@@ -1,8 +1,15 @@
 const { SlashCommandBuilder } = require("discord.js");
+const {
+  questionChannelName,
+  notifyAboutQuestionsRole,
+} = require("./../../bot-data.json");
 
+let notifyMonitorsStatusMessage = "";
 const newQuestionCommand = new SlashCommandBuilder()
   .setName("newquestion")
-  .setDescription("Creates a new question in the 'Questions' channel")
+  .setDescription(
+    `Creates a new question in the '${questionChannelName}' channel`
+  )
   .addStringOption((question) => {
     return question
       .setName("question")
@@ -14,12 +21,12 @@ function getQuestionsChannel(server) {
   const serverChannels = server.channels.cache;
 
   const questionsChannel = serverChannels.find((channel) => {
-    return channel.name == "questions";
+    return channel.name == questionChannelName;
   });
 
   if (!questionsChannel) {
     throw Error(
-      "A channel with name 'Questions' was not found, create a new one"
+      `A channel with name '${questionChannelName}' was not found, create a new one`
     );
   }
 
@@ -47,7 +54,7 @@ function getAvailableMonitors(serverMembers) {
     const memberRoles = member.roles.cache;
 
     const memberIsMonitor = memberRoles.some((role) => {
-      return role.name == "Monitor";
+      return role.name == notifyAboutQuestionsRole;
     });
 
     return memberIsAvailable && memberIsMonitor;
@@ -63,12 +70,19 @@ async function notifyAvailableMonitors(interactionData) {
   const serverMembers = await interactionData.guild.members.fetch();
 
   const availableMonitors = getAvailableMonitors(serverMembers);
+  const noMonitorsAvailable = availableMonitors.size == 0;
 
   availableMonitors.forEach(async (monitor) => {
     const notifyMessage = `@${questionSenderId} sent this new question: ${newQuestion}`;
 
     await monitor.send(notifyMessage);
   });
+
+  if (noMonitorsAvailable) {
+    notifyMonitorsStatusMessage = `There are no members with the role '${notifyAboutQuestionsRole}' available now`;
+  } else {
+    notifyMonitorsStatusMessage = `The questions was submitted to the available members with the role '${notifyAboutQuestionsRole}'.'`;
+  }
 }
 
 async function handleNewQuestion(interactionData) {
@@ -84,8 +98,11 @@ async function handleNewQuestion(interactionData) {
   await notifyAvailableMonitors(interactionData);
 
   await interactionData.reply(
-    "The question was posted in the channels questions and have also been submited to the available monitors"
+    notifyMonitorsStatusMessage +
+      `\nThe question was posted in the ${questionChannelName} channel.`
   );
+
+  notifyMonitorsStatusMessage = "";
 }
 
 module.exports = {
